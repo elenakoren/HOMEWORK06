a137ff1975baf63be39e10d6fd24399c3fa4e16b
Elena Korenko <elenakor987@gmail.com>
2022-12-26 22:03:52 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввёл
+пользователь.
+0, 7, 8,-2,-2 -> 2
+1,-7, 567, 89, 223-> 3*/
+
+int GetNumber(string message)
+{
+    int result;
+    int count=0;
+    
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(),out result))
+        {
+            Console.WriteLine("Введите числа ");
+            if(result>0)
+            {
+                count++;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+int number = GetNumber("Введите число,или любой символ если закончили ввод");
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0={number}");
